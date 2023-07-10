9cbf69c6ab33cfd4854ba3de56821861c9ba9d95
k0723 <kimbum7824@gmail.com>
2023-07-10 21:23:02 +0900

    feat : 게시글 업데이트, 삭제 추가

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,8 @@ const index = require("../models");
 const users = require("../models").users;
 const posts = require("../models").posts;
 const authMiddleware = require('../middleware/authMiddleware');
+const { Op } = require("sequelize");
 
-// localhost:3000/api/about GET
 router.post("/posts",authMiddleware, async (req, res) => {
   console.log("test")
   const { userId } = res.locals.user;
@@ -37,7 +37,6 @@ router.get("/posts", async (req, res) => {
 
 router.get("/posts/:postId", async (req, res) => {
   const { postId } = req.params;
-  console.log(postId);
   const post = await posts.findOne({
     attributes: ["postId",'title', 'content', 'createdAt'],
     where : { postId }
@@ -46,13 +45,51 @@ router.get("/posts/:postId", async (req, res) => {
   });
 
 
-router.put("/posts/:postId", (req, res) => {
-    
-    res.send("goods.js about PATH");
+router.put("/posts/:postId", authMiddleware, async (req, res) => {
+    const { postId } = req.params;
+    const { userId } = res.locals.user;
+    const { title, content } = req.body;
+  
+    const post = await posts.findOne({ where: { postId } });
+  
+    if (!post) {
+      return res.status(404).json({ message: "게시글이 존재하지 않습니다." });
+    } else if (post.userId !== userId) {
+      return res.status(401).json({ message: "권한이 없습니다." });
+    }
+  
+    await posts.update(
+      { title, content }, 
+      {
+        where: {
+          [Op.and]: [{ postId }, { userId: userId }],
+        }
+      }
+    );
+  
+    return res.status(200).json({ data: "게시글이 수정되었습니다." });
   });
 
-router.delete("/posts/:postId", (req, res) => {
-    res.send("goods.js about PATH");
+  router.delete("/posts/:postId", authMiddleware, async (req, res) => {
+    const { postId } = req.params;
+    const { userId } = res.locals.user;
+  
+    const post = await posts.findOne({ where: { postId } });
+  
+    if (!post) {
+      return res.status(404).json({ message: "게시글이 존재하지 않습니다." });
+    } else if (post.userId !== userId) {
+      return res.status(401).json({ message: "권한이 없습니다." });
+    }
+  
+    await posts.destroy({
+      where: {
+        [Op.and]: [{ postId }, { userId: userId }],
+      }
+    });
+  
+    return res.status(200).json({ data: "게시글이 삭제되었습니다." });
   });
+  
 
   module.exports = router
